7d24bc7894aa9e0eaadc826936b6bfc65a0a0a35
Oliver <oc@hejverden.dk>
2017-11-14 15:46:20 +0100

    Build #inside_board?, #possible_move? and a v1 of #build_tree

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,21 +1,55 @@
-class Tree
-end
+class Tile
+  attr_accessor :data, :parent, :children
 
-class Node
+  def initialize(data, parent = nil)
+    @data = data
+    @parent = parent
+    @children = []
+  end
 end
 
 class Knight
-  move_patterns = [[1, 2], [2, 1], [2, -1], [1, -2],
-                  [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  attr_accessor :root, :visited
 
-  def legal_move?(from, to)
+  def initialize(root, target)
+    @target = target
+    @root = Tile.new(root)
+    @tree = [@root]
+    @visited = [@root]
+    @move_patterns = [[1, 2], [2, 1], [2, -1], [1, -2],
+                    [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+  end
+
+  def build_tree(tile_ary)
+    tile_ary.each do |tile|
+      @move_patterns.each do |pattern|
+        child = [tile.data[0] + pattern[0], tile.data[1] + pattern[1]]
+        tile.children << Tile.new(child, tile) if possible_move?(tile.data, child)
+      end
+    end
+  end
+
+  def inside_board?(from, to)
     # Check if the move goes outside the board
     (from[0] + to[0]).between?(0, 7) && (from[1] + to[1]).between?(0, 7)
   end
 
-  def possible_moves
-    
+  def possible_move?(from, to)
+    inside_board?(from, to) && !@visited.include?(to)
   end
 
+  def test
+    build_tree(@tree)
+  end
+
+  def show_tree
+    puts @tree.inspect
+  end
 end
+
+knight = Knight.new([3,3], [5,7])
+knight.show_tree
+puts " "
+knight.test
+knight.show_tree
 
